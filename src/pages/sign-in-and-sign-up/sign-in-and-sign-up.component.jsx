b492c3410c15a5775fef95c413cc8a1117981e4a
Andrew <areddin409@gmail.com>
2020-02-07 17:28:53 -0600

    adding sign-up form

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-sign-up.styles.scss';
+import Signin from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <Signin />
+  </div>
+);
+
+export default SignInAndSignUpPage;
